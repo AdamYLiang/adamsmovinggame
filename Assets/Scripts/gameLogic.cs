c150fe5f1cd45a2f6b3d79329ce87150f49c462f
unknown <ayl292@nyu.edu>
2016-02-25 15:32:24 -0500

    working basic box logic, adding more variety in boxes now

---
+++ Assets/Scripts/gameLogic.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class gameLogic : MonoBehaviour {
+
+	public Text hintHelp;
+	public Transform player;
+	public Transform fastBox;
+	public Transform heavyBox;
+	//bool hasBox = false;
+	//int boxType = 0;
+
+	// Use this for initialization
+	void Start () {
+
+		//GameObject player = GameObject.Find("Player");
+		//playerMovement playerMovement = player.GetComponent<playerMovement>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		string textBuffer = " ";
+
+		if((player.position - fastBox.position).magnitude < 3f){
+			textBuffer = "Press [SPACE] to pick up a light box";
+
+			if(Input.GetKeyDown(KeyCode.Space)){
+				hasBox = true;
+				boxType = 1;
+				player.GetComponent<playerMovement>().hasBox = true;
+				player.GetComponent<playerMovement>().boxType = "fast";
+			}
+
+
+		}
+			
+		hintHelp.text = textBuffer;
+		
+	}
+}
--- Assets/Scripts/playerMovement.cs
+++ Assets/Scripts/playerMovement.cs
@@ -1,9 +1,17 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class playerMovement : MonoBehaviour {
 
 	CharacterController myCharController;
+	public Text holdingHelper;
+
+	//Instead of having multiple booleans, have one that if true it equates the type of box
+	//So boxes 1, 2, 3, 4 instead of 4 booleans. 
+	public bool hasBox = false;
+	public string boxType = "none";
+	public float speed = 5f;
 
 	// Use this for initialization
 	void Start () {
@@ -15,11 +23,28 @@ public class playerMovement : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
+		string textBuffer = "Holding: \n";
+
+		//General Movement
 		float horizontal = Input.GetAxis("Horizontal");
 		float vertical = Input.GetAxis("Vertical");
-		Vector3 movement = transform.forward * 5f * vertical;
-
+		Vector3 movement = transform.forward * speed * vertical;
 		myCharController.Move((movement + Physics.gravity) * Time.deltaTime);
 		transform.Rotate(0f, horizontal * 90f * Time.deltaTime, 0f);
+
+		if(hasBox){
+
+			if(boxType == "fast"){
+				textBuffer += "Fast Box";
+				speed = 15f;
+			}
+			
+		}
+
+		else{
+			textBuffer += "Nothing";
+		}
+
+		holdingHelper.text = textBuffer;
 	}
 }
