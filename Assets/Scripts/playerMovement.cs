8932c3e08e7bc96a2128cfa7e523c4a14c107fc5
unknown <ayl292@nyu.edu>
2016-02-25 14:51:40 -0500

    more initialization, moving player

---
+++ Assets/Scripts/playerMovement.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playerMovement : MonoBehaviour {
+
+	CharacterController myCharController;
+
+	// Use this for initialization
+	void Start () {
+
+		myCharController = GetComponent<CharacterController>();
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		float horizontal = Input.GetAxis("Horizontal");
+		float vertical = Input.GetAxis("Vertical");
+		Vector3 movement = transform.forward * 5f * vertical;
+
+		myCharController.Move((movement + Physics.gravity) * Time.deltaTime);
+		transform.Rotate(0f, horizontal * 90f * Time.deltaTime, 0f);
+	}
+}
